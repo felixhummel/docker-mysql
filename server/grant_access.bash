061d354903e02bc1b84d9b5d4b11a54945dc379a
Felix Hummel <felix@felixhummel.de>
2014-01-26 19:06:19 +0100

    simplify grant_access.bash

--- server/grant_access.bash
+++ server/grant_access.bash
@@ -3,26 +3,12 @@
 username=changeme
 password=changeme
 
-socket=/var/run/mysqld/mysqld.sock
-startup_timeout_in_seconds=10
-t0=$(date +%s)
-echo "t0=$t0"
-t1=$((t0 + startup_timeout_in_seconds))
-echo "t1=$t1"
-
-# daemon in background so we can use the client to allow remote access
-mysqld_safe &
-
-while true; do
-  t=$(date +%s)
-  echo "t=$t"
-  if [[ -S $socket ]]; then
-    echo "grant all on *.* to '$username'@'%' identified by '$password'; flush privileges;" | mysql
-    break
-  fi
-  sleep 1
-  [[ $t > $t1 ]] && break
-done
-
+# need to flush first (http://bugs.mysql.com/bug.php?id=38513)
+# and then flush to make the changes take effect
+mysqld --bootstrap<<EOF
+flush privileges;
+grant all on *.* to '$username'@'%' identified by '$password';
+flush privileges;
+EOF
 exit 0
 
